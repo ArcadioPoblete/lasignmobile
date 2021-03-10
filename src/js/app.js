a2c52a520300d0a19346b310fb7e6dfde1754fec
Arcadio <apt300485@gmail.com>
2021-03-10 17:01:12 -0600

    Activación de menu
    Menu de navegación mobil

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,13 @@ import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import '../../node_modules/@fortawesome/fontawesome-free/js/all.js';
 import  '../../node_modules/headroom.js/dist/headroom.js';
 import './components/components.js';
+
+
+const picker = document.querySelectorAll('.datePicker');
+
+picker.forEach((item) => {
+    const datepicker = new Datepicker(item, {
+      
+  });
+});
+
---
+++ src/js/select-fechas.js
@@ -0,0 +1,8 @@
+
+const picker = document.querySelectorAll('.datePicker');
+
+picker.forEach((item) => {
+    const datepicker = new Datepicker(item, {
+      
+  });
+});
